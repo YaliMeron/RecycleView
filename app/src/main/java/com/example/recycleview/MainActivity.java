83d1f9ee1d2c5e8f1d0ebd5d8625dc43b1483b12
YaliMeron <yalimeron10@gmail.com>
2024-02-20 11:10:55 +0200

    End of Lesson. HomeWork

--- app/src/main/java/com/example/recycleview/MainActivity.java
+++ app/src/main/java/com/example/recycleview/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.recycleview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -16,10 +18,19 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.icon44};
 
     ArrayList<CardModel> models = new ArrayList<>();
+
+    CardAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpModels();
+        adapter = new CardAdapter(this, models);
+        RecyclerView recyclerView = findViewById(R.id.Rview);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
     }
 
     private void setUpModels() {
